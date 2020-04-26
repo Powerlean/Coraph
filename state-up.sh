e2cbb137008dc0b03f771f094f63bbf280c3b969
Solaris <bluereeze191@gmail.com>
2020-04-26 13:09:35 +0800

    SERF(unstable)

---
+++ state-up.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#coding:UTF-8
+
+
+#some color
+yellow="\033[33m"
+red="\033[31m"
+green="\033[32m"
+suffix="\033[0m"
+
+
+#check environment
+echo -e "${green} Checking environment... ${suffix}"
+check=`which wget`
+if [ "x"$check == x ]; then
+apt install wget -y
+fi
+
+#remove older package
+rm -rf $PREFIX/bin/coraph*
+rm -rf $PREFIX/etc/coraph.conf*
+
+
+#start download
+echo -e "${green} Downloading... ${suffix}"
+wget "https://raw.githubusercontent.com/EdgeS5352/Coraph/master/coraph"
+wget "https://raw.githubusercontent.com/EdgeS5352/Coraph/master/coraph.conf"
+
+
+#start state
+echo -e "${green} Setting... ${suffix}"
+mv coraph $PREFIX/bin
+mv coraph $PREFIX/etc
+chmod +x $PREFIX/bin/coraph
+echo -e "{green} Getting ready {suffix}"
+coraph
+coraph help
